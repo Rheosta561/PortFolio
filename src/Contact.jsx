8d9f325ffdccb179d145792c9a4bc50895bf73d2
Rheosta561 <manubhav731@gmail.com>
2024-11-20 22:08:27 +0530

    Aligned Contacts

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
       
       <div className='text-6xl text-zinc-950 font-semibold'>Contacts</div>
       <div className='flex -mt-2'>
-        <div className='pl-16 text-gray-700 font-semibold mt-4' >Phone</div>
+        <div className='pl-4 text-gray-700 font-semibold mt-4' >Phone</div>
         <div className='pl-4 text-sm font-semibold mt-5 text-gray-800'><a 
     href="tel:+917303036689" 
     className="text-gray-900 hover:underline"
@@ -15,7 +15,7 @@ function Contact() {
   </a> </div>
       </div>
       <div className='flex -mt-2'>
-        <div className='pl-16 text-gray-700 font-semibold mt-2' >Email</div>
+        <div className='pl-4 text-gray-700 font-semibold mt-2' >Email</div>
         <div className='pl-6 text-sm font-semibold mt-3 text-gray-800'><a 
     href="mailto:manubhav731@gmail.com" 
     className="text-gray-900 hover:underline"
@@ -25,11 +25,11 @@ function Contact() {
       </div>
       <div className='flex -mt-2'>
         <div className=' rounded-full bg-zinc-950'></div>
-        <div className='pl-16  font-semibold mt-2 text-blue-800'><a href="https://www.linkedin.com/in/anubhav-mishra-2b8175285/">LinkedIn</a></div>
+        <div className='pl-4  font-semibold mt-2 text-blue-800'><a href="https://www.linkedin.com/in/anubhav-mishra-2b8175285/">LinkedIn</a></div>
       </div>
       <div className='flex -mt-2'>
         <div ></div>
-        <div className='pl-16  font-semibold mt-2 text-emerald-800'><a href="https://github.com/Rheosta561">GitHub</a></div>
+        <div className='pl-4  font-semibold mt-2 text-emerald-800'><a href="https://github.com/Rheosta561">GitHub</a></div>
       </div>
       
     </div>
